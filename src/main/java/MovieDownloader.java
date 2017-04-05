27a18096df0c828e6d654bbfdde04b867f684c1b
Kar Yin Ng <karyin3@uw.edu>
2017-04-04 21:11:19 -0700

    Add comments

--- src/main/java/MovieDownloader.java
+++ src/main/java/MovieDownloader.java
@@ -22,33 +22,34 @@ public class MovieDownloader {
 
 		//construct the url for the omdbapi API
 		String urlString = "";
-		try {
+		try { // if an exception occurs within this try block, the exception is handled by the catch block (exception handler)
 			urlString = "http://www.omdbapi.com/?s=" + URLEncoder.encode(movie, "UTF-8") + "&type=movie";
-		}catch(UnsupportedEncodingException uee){
+		}catch(UnsupportedEncodingException uee){ // an exception handler that handles the type of exception indicated by its argument
 			return null;
 		}
 
 		HttpURLConnection urlConnection = null;
 		BufferedReader reader = null;
 
-		String movies[] = null;
+		String[] movies = null;
 
 		try {
 
 			URL url = new URL(urlString);
 
-			urlConnection = (HttpURLConnection) url.openConnection();
-			urlConnection.setRequestMethod("GET");
-			urlConnection.connect();
+			urlConnection = (HttpURLConnection) url.openConnection(); //a connection to the remote object referred to by the URL
+			urlConnection.setRequestMethod("GET"); //Set the method for URL request
+			urlConnection.connect(); //Opens a communications link to the resource reference by the URL
 
-			InputStream inputStream = urlConnection.getInputStream();
+			InputStream inputStream = urlConnection.getInputStream(); // an input stream of bytes
 			StringBuffer buffer = new StringBuffer();
 			if (inputStream == null) {
 				return null;
 			}
+			//Buffers characters for efficient reading of characters, arrays and lines.
 			reader = new BufferedReader(new InputStreamReader(inputStream));
 
-			String line = reader.readLine();
+			String line = reader.readLine(); //Reads a line of text
 			while (line != null) {
 				buffer.append(line + "\n");
 				line = reader.readLine();
@@ -62,18 +63,18 @@ public class MovieDownloader {
 			results = results.replace("]}","");
 			results = results.replace("},", "},\n");
 
-			movies = results.split("\n");
+			movies = results.split("\n"); //returns an array of movies
 		} 
 		catch (IOException e) {
 			return null;
 		} 
-		finally {
+		finally { // contains cleanup code and closes the connection
 			if (urlConnection != null) {
-				urlConnection.disconnect();
+				urlConnection.disconnect(); //indicates that other requests to the server are unlikely in the near future
 			}
 			if (reader != null) {
 				try {
-					reader.close();
+					reader.close(); //close input stream
 				} 
 				catch (IOException e) {
 				}
